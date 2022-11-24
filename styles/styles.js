ce43282d0cc289e75f29b1ee3da4681ced3aa6b8
Salman Hasan <96196317+salman1hasan@users.noreply.github.com>
2022-11-24 01:17:41 -0500

    Add files via upload

---
+++ styles/styles.js
@@ -0,0 +1,33 @@
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles({
+  navbar: {
+    backgroundColor: '#203040',
+    '& a': {
+      color: '#ffffff',
+      marginLeft: 10,
+    },
+  },
+  brand: {
+    fontweight: 'bold',
+    fontSize: '1.5rem',
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  main: {
+    minHeight: '80vh',
+  },
+  footer: {
+    textAlign: 'center',
+  },
+  section: {
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  footer: {
+    marginTop: 10,
+    textAlign: 'center',
+  },
+});
+export default useStyles;
