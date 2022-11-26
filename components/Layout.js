10d3ca0d94845c7e4320a3ea51f07941cb9249d9
Salman Hasan <96196317+salman1hasan@users.noreply.github.com>
2022-11-26 00:17:16 -0500

    Add files via upload

--- components/Layout.js
+++ components/Layout.js
@@ -7,10 +7,39 @@ import {
   Typography,
   Container,
   Link,
+  ThemeProvider,
+  createMuiTheme,
+  CssBaseline,
 } from '@material-ui/core';
 import useStyles from '../utils/styles';
 
 export default function Layout({ title, description, children }) {
+  const theme = createMuiTheme({
+    typography: {
+      h1: {
+        fontSize: '1.6rem',
+        fontWeight: 400,
+        margin: '1rem 0',
+      },
+      h2: {
+        fontSize: '1.4rem',
+        fontWeight: 400,
+        margin: '1rem 0',
+      },
+      body1: {
+        fontWeight: 'normal',
+      },
+    },
+    palette: {
+      type: 'light',
+      primary: {
+        main: '#f0c000',
+      },
+      secondary: {
+        main: '#208080',
+      },
+    },
+  });
   const classes = useStyles();
   return (
     <div>
@@ -20,26 +49,29 @@ export default function Layout({ title, description, children }) {
         </title>
         {description && <meta name="description" content={description}></meta>}
       </Head>
-      <AppBar position="static" className={classes.navbar}>
-        <Toolbar>
-          <Link href="/" passhref>
-            <Typography className={classes.brand}>amazona</Typography>
-          </Link>
-          <div className={classes.grow}></div>
-          <div>
-            <Link href="/cart" passHref>
-              Cart
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <AppBar position="static" className={classes.navbar}>
+          <Toolbar>
+            <Link href="/" passhref>
+              <Typography className={classes.brand}>amazona</Typography>
             </Link>
-            <Link href="/login" passHref>
-              Login
-            </Link>
-          </div>
-        </Toolbar>
-      </AppBar>
-      <Container className={classes.main}>{children}</Container>
-      <footer className={classes.footer}>
-        <Typography>All rights reserved Next Amazona.</Typography>
-      </footer>
+            <div className={classes.grow}></div>
+            <div>
+              <Link href="/cart" passHref>
+                Cart
+              </Link>
+              <Link href="/login" passHref>
+                Login
+              </Link>
+            </div>
+          </Toolbar>
+        </AppBar>
+        <Container className={classes.main}>{children}</Container>
+        <footer className={classes.footer}>
+          <Typography>All rights reserved Next Amazona.</Typography>
+        </footer>
+      </ThemeProvider>
     </div>
   );
 }
