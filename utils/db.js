35329c4071a3c0458bcea15c71a7f1f410636bf4
Salman Hasan <96196317+salman1hasan@users.noreply.github.com>
2022-11-30 23:56:35 -0500

    Update db.js

--- utils/db.js
+++ utils/db.js
@@ -17,7 +17,7 @@ async function connect() {
     await mongoose.disconnect();
   }
   const db = await mongoose.connect(
-    'mongodb+srv://salmanhasan:salmanhasan@cluster0.mabf423.mongodb.net/?retryWrites=true&w=majority'
+    
   );
   console.log('new connection');
   connection.isConnected = db.connections[0].readyState;
