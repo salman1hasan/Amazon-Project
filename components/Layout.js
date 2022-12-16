d2b4cc63d9d208f190f919d15e5221eb7505cc9d
Salman Hasan <96196317+salman1hasan@users.noreply.github.com>
2022-12-16 01:01:07 -0500

    Add files via upload

--- components/Layout.js
+++ components/Layout.js
@@ -1,91 +1,99 @@
-import React, { useContext } from 'react';
-import Head from 'next/head';
-import NextLink from 'next/link';
-import {
-  AppBar,
-  Toolbar,
-  Typography,
-  Container,
-  Link,
-  ThemeProvider,
-  createMuiTheme,
-  CssBaseline,
-  Switch,
-} from '@material-ui/core';
-import useStyles from '../utils/styles';
-import { Store } from '../utils/Store';
+import React, { useContext, useEffect, useState } from 'react';
+import 'react-toastify/dist/ReactToastify.css';
+import { signOut, useSession } from 'next-auth/react';
+import { ToastContainer } from 'react-toastify';
 import Cookies from 'js-cookie';
+import Head from 'next/head';
+import Link from 'next/link';
+import { Store } from '../utils/Store';
+import { Menu } from '@headlessui/react';
+import DropdownLink from './DropdownLink';
 
-export default function Layout({ title, description, children }) {
+export default function Layout({ title, children }) {
+  const { status, data: session } = useSession();
   const { state, dispatch } = useContext(Store);
-  const { darkMode } = state;
-  const theme = createMuiTheme({
-    typography: {
-      h1: {
-        fontSize: '1.6rem',
-        fontWeight: 400,
-        margin: '1rem 0',
-      },
-      h2: {
-        fontSize: '1.4rem',
-        fontWeight: 400,
-        margin: '1rem 0',
-      },
-      body1: {
-        fontWeight: 'normal',
-      },
-    },
-    palette: {
-      type: darkMode ? 'dark' : 'light',
-      primary: {
-        main: '#f0c000',
-      },
-      secondary: {
-        main: '#208080',
-      },
-    },
-  });
-  const classes = useStyles();
-  const darkModeChangeHandler = () => {
-    dispatch({ type: darkMode ? 'DARK_MODE_OFF' : 'DARK_MODE_ON' });
-    const newDarkMode = !darkMode;
-    Cookies.set('darkMode', newDarkMode ? 'ON' : 'OFF');
+  const { cart } = state;
+  const [cartItemsCount, setCartItemsCount] = useState(0);
+  useEffect(() => {
+    setCartItemsCount(cart.cartItems.reduce((a, c) => a + c.quantity, 0));
+  }, [cart.cartItems]);
+
+  const logoutClickHandler = () => {
+    Cookies.remove('cart');
+    dispatch({ type: 'CART_RESET' });
+    signOut({ callbackUrl: '/login' });
   };
   return (
-    <div>
+    <>
       <Head>
-        <title>
-          {title ? `${title}-Next Amazona` : 'Next Amazona'} Next Amazona
-        </title>
-        {description && <meta name="description" content={description}></meta>}
+        <title>{title ? title + ' - Amazona' : 'Amazona'}</title>
+        <meta name="description" content="Ecommerce Website" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <AppBar position="static" className={classes.navbar}>
-          <Toolbar>
-            <Link href="/" passhref>
-              <Typography className={classes.brand}>amazona</Typography>
+
+      <ToastContainer position="bottom-center" limit={1} />
+      <div className="flex min-h-screen flex-col justify-between">
+        <header>
+          <nav className="flex h-12 items-center px-4 justify-between shadow-md">
+            <Link href="/">
+              <a className="text-lg font-bold">Amazona</a>
             </Link>
-            <div className={classes.grow}></div>
             <div>
-              <Switch
-                checked={darkMode}
-                onChange={darkModeChangeHandler}
-              ></Switch>
-              <Link href="/cart" passHref>
-                Cart
+              <Link href="/cart">
+                <a className="p-2">
+                  Cart
+                  {cartItemsCount > 0 && (
+                    <span className="ml-1 rounded-full bg-red-600 px-2 py-1 text-xs font-bold text-white">
+                      {cartItemsCount}
+                    </span>
+                  )}
+                </a>
               </Link>
-              <Link href="/login" passHref>
-                Login
-              </Link>
+              {status === 'loading' ? (
+                'Loading'
+              ) : session?.user ? (
+                <Menu as="div" className="relative inline-block">
+                  <Menu.Button className="text-blue-600">
+                    {session.user.name}
+                  </Menu.Button>
+                  <Menu.Items className="absolute right-0 w-56 origin-top-right bg-white shadow-lg">
+                    <Menu.Item>
+                      <DropdownLink className="dropdown-link" href="/profile">
+                        Profile
+                      </DropdownLink>
+                    </Menu.Item>
+                    <Menu.Item>
+                      <DropdownLink
+                        className="dropdown-link"
+                        href="/order-history"
+                      >
+                        Order History
+                      </DropdownLink>
+                    </Menu.Item>
+                    <Menu.Item>
+                      <a
+                        className="dropdown-link"
+                        href="#"
+                        onClick={logoutClickHandler}
+                      >
+                        Logout
+                      </a>
+                    </Menu.Item>
+                  </Menu.Items>
+                </Menu>
+              ) : (
+                <Link href="/login">
+                  <a className="p-2">Login</a>
+                </Link>
+              )}
             </div>
-          </Toolbar>
-        </AppBar>
-        <Container className={classes.main}>{children}</Container>
-        <footer className={classes.footer}>
-          <Typography>All rights reserved Next Amazona.</Typography>
+          </nav>
+        </header>
+        <main className="container m-auto mt-4 px-4">{children}</main>
+        <footer className="flex h-10 justify-center items-center shadow-inner">
+          <p>Copyright @2022 Amazona</p>
         </footer>
-      </ThemeProvider>
-    </div>
+      </div>
+    </>
   );
 }
